e051b952721b653568e6d0da05ccf7fc72c24e75
fengyqf <fengyqf@gmail.com>
2016-01-20 16:06:35 +0800

    db sql, without autocrement count, in create table

--- minimisation.sql
+++ minimisation.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `mnms_allocation` (
   PRIMARY KEY (`id`),
   KEY `group_id` (`group_id`),
   KEY `time` (`time`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=124 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `mnms_allocation2layer` (
   `layer_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `allocation_layer` (`allocation_id`,`layer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=326 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `mnms_factor` (
   `weight` int(11) NOT NULL DEFAULT '1' COMMENT '权重比值',
   PRIMARY KEY (`id`),
   KEY `study_id` (`study_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,8 +74,9 @@ CREATE TABLE IF NOT EXISTS `mnms_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '',
   `study_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=36 ;
+  PRIMARY KEY (`id`),
+  KEY `study_id` (`study_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `mnms_layer` (
   `factor_id` int(11) NOT NULL DEFAULT '0' COMMENT 'fk to: factor.id',
   PRIMARY KEY (`id`),
   KEY `factor_id` (`factor_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -105,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `mnms_study` (
   `owner_uid` int(11) NOT NULL DEFAULT '0' COMMENT '所有者id号，可以任意外部用户系统的uid',
   `time` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2013 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
